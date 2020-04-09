a73656eac654b4061b1fe248cb407c41b9a82601
Oli <osmith@netvirta.com>
2020-04-09 22:12:32 +0800

    Update docker apt repo url and gpg key

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -25,6 +25,7 @@ class kubernetes::repos (
     case $facts['os']['family']  {
       'Debian': {
         $codename = fact('os.distro.codename')
+        $distro = downcase(fact('os.distro.id'))
         apt::source { 'kubernetes':
           location => pick($kubernetes_apt_location,'https://apt.kubernetes.io'),
           repos    => pick($kubernetes_apt_repos,'main'),
@@ -37,12 +38,12 @@ class kubernetes::repos (
 
           if $container_runtime == 'docker' and $manage_docker == true {
             apt::source { 'docker':
-              location => pick($docker_apt_location,'https://apt.dockerproject.org/repo'),
-              repos    => pick($docker_apt_repos,'main'),
-              release  => pick($docker_apt_release,"ubuntu-${codename}"),
+              location => pick($docker_apt_location,"https://download.docker.com/linux/$distro"),
+              repos    => pick($docker_apt_repos,'stable'),
+              release  => pick($docker_apt_release,"${codename}"),
               key      => {
-                'id'     => pick($docker_key_id,'58118E89F3A912897C070ADBF76221572C52609D'),
-                'source' => pick($docker_key_source,'https://apt.dockerproject.org/gpg'),
+                'id'     => pick($docker_key_id,'9DC858229FC7DD38854AE2D88D81803C0EBFCD88'),
+                'source' => pick($docker_key_source,"https://download.docker.com/linux/$distro/gpg"),
             },
           }
         }
